8387ef4241074a89be922c5d8038aa3215e14397
Nils <nils.brynedal@gmail.com>
2022-06-12 19:27:14 +0200

    Added more tests for any implementations of IDatabase

--- TODOHandler_Tests/DatabaseTests.cs
+++ TODOHandler_Tests/DatabaseTests.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
 using TODOHandler;
 
 namespace TODOHandler_Tests
@@ -34,6 +32,37 @@ namespace TODOHandler_Tests
             Assert.AreEqual(1, result.Count);
             Assert.AreEqual("Test", result[0].Description);
             Assert.AreEqual("2022-01-12", result[0].Due);
+        }
+
+        [TestMethod]
+        public void ShallAddTwoTasks()
+        {
+            GetInstance().Add("Test", "2022-01-12");
+            GetInstance().Add("Test2", "2022-01-13");
+
+            var result = GetInstance().ReadAllTasks();
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("Test2", result[1].Description);
+            Assert.AreEqual("2022-01-13", result[1].Due);
+        }
+
+        [TestMethod]
+        public void ShallCompleteOneTask()
+        {
+            GetInstance().Add("Test", "2022-01-12");
+            GetInstance().Add("Test2", "2022-01-13");
+            GetInstance().CompleteTask(1);
+
+            var result = GetInstance().ReadAllTasks();
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("Test", result[0].Description);
+            Assert.AreEqual("Complete", result[0].Due);
+
+            //should not have changed:
+            Assert.AreEqual("Test2", result[1].Description);
+            Assert.AreEqual("2022-01-13", result[1].Due);
         }
     }
 }
